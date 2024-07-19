51d77ba5abdc619b3b59399cd71715680be59c27
Ekaterina-byte <max-ka82@mail.ru>
2024-07-19 21:14:10 +0300

    Add files via upload

---
+++ hmework4.py
@@ -0,0 +1,8 @@
+my_string =input('Введите Ваше имя ')
+print(len(my_string))
+name = 'Не Боги горшки обжигают'
+print(name.upper())
+print(name.lower())
+print(name.replace(' ',''))
+print(name[0])
+print(name[-1])
